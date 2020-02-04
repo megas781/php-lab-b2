d96d4a40bfcbf04cc5ab8ebba4a903144440540e
glebkalachev <megas781@gmail.com>
2020-02-05 00:30:42 +0300

    Initial interface implemented

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 require_once 'app-start.php';
 
 
+
 ?>
 
 <?php require SITE_ROOT . 'master-page/Header/header.php'; ?>
 
-<h1>Здесь будет контент</h1>
+<form class="_flex-centering column calc-container">
+    <div class="output">0</div>
+    <div><input type="text" class="input-field"></div>
+<!--    <textarea name="input" rows="2" placeholder="(2 + 2) * 2" class="input-field"></textarea>-->
+    <div><input type="submit" class="resolve-button" value="Resolve"></div>
+
+</form>
 
 <?php require SITE_ROOT . 'master-page/Footer/footer.php' ?>
 
